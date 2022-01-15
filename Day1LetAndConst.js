a6360fc20b09f022ef1f7b48e1ba4b70691d57e5
Buğra Öner <boletroy@gmail.com>
2022-01-15 14:17:08 +0300

    Day 1 ..

---
+++ Day1LetAndConst.js
@@ -0,0 +1,13 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    
+    let r = readLine();
+    
+    
+    // Print the area of the circle:
+    
+    console.log(PI*r*r);
+    console.log(PI*2*r);
+
+    
