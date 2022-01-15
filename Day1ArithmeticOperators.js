c2e365aadd4b0c5a6a9228537fd9b2c18b9b7a36
Buğra Öner <boletroy@gmail.com>
2022-01-15 14:17:18 +0300

    Day 1 ...

---
+++ Day1ArithmeticOperators.js
@@ -0,0 +1,38 @@
+function getArea(length, width) {
+    let area;
+    area =width*length;
+    
+    
+    return area;
+    
+}
+
+
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    
+    perimeter= 2*(width+length);
+    
+   
+     
+    return perimeter;
+    
+    
+ 
+    
+    
+    
+   
+    
+  
+    
+}
