18e3982d9bfa22d6d9460cd7fed5e868c89b15aa
Buğra Öner <boletroy@gmail.com>
2022-01-28 03:09:17 +0300

    Day 4

---
+++ Days4CreateArectangle.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    
+    return {
+        length : a,
+        width : b,
+        perimeter : 2 * (a +b),
+        area : a*b,
+    }
+    
+
+    /*
+
+function Rectangle(a, b) {
+  this.length = a;
+  this.width = b;
+  this.area = a * b;
+  this.perimeter = 2 * (a + b);
+}
+
+
+
+    */
+   
+}
+
